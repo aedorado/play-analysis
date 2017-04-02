09e0689df029c31d5f2d586e649a0ebbefcf5109
Anurag El Dorado <annurag94@gmail.com>
2017-04-02 21:24:38 +0000

    Getting close

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,6 +1,7 @@
 from URL import URL
 from DB import DB
 from bs4 import BeautifulSoup
+import re
 
 url = URL('https://play.google.com/store/apps/details?id=com.bsb.hike')
 html = url.fetch()
@@ -13,7 +14,23 @@ id_app_title = id_app_title.text
 print id_app_title
 
 document_subtitles = soup.findAll("div", { "class" : "document-subtitles" })
-print document_subtitles
+# print document_subtitles
+
+re_name = re.compile('<span itemprop="name">.*?<\/span>')
+name = re_name.findall(html)
+name = name[0]
+name = re.sub(r"<.*?>", "", name)
+print name
+
+re_genre = re.compile('<span itemprop="genre">.*?<\/span>')
+genre = re_genre.findall(html)
+genre = genre[0]
+genre = re.sub(r"<.*?>", "", genre)
+print genre
+
+bar_numbers = soup.findAll("span", { "class" : "bar-number" })
+print bar_numbers
+
 
 rec_cluster = soup.findAll("div", { "class" : "rec-cluster" })
 see_more = rec_cluster[0].find('a')
