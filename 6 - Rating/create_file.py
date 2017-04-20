fb087abda04288633257eb1d64558c9b61ef08cb
El Dorado <annurag94@gmail.com>
2017-04-20 17:52:30 +0530

    Created rating file.

---
+++ 6 - Rating/create_file.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../commons'))
+from DB import DB
+
+def create():
+	db = DB('../db/play.db')
+	f = open('rating.txt', 'w')
+	all_meta = db.qry('select metadata.id, genre, (one + two * 2 + three * 3 + four * 4 + five * 5.0)/(one + two + three + four + five) as avgr, (one + two + three + four + five) as numr from metadata, rating WHERE metadata.id=rating.id ORDER BY (avgr * numr) DESC;')
+	
+	count = 0
+	for meta in all_meta:
+		f.write('"' + meta[0] + '"' + ' "' + meta[1] + '" ' + str(meta[2]) + ' ' + str(meta[3]) + '\n')
+		count = count + 1
+
+	f.close()
+
+	
+if __name__ == "__main__":
+	create()
